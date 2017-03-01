c9774299047d402c172ad37434f81c22d3991c14
Jos Hickson <j.hickson@winton.com>
2017-03-01 16:10:31 +0000

    Added demo code.

---
+++ src/Winton.Blogs.TPLActor/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Winton. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENCE in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Winton")]
+[assembly: AssemblyProduct("Winton.Blogs.TPLActor")]
